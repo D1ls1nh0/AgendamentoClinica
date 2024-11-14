d6c1cd52d287da595434f30d18cd26add5f8d361
D1ls1nh0 <adilsondenis1@gmail.com>
2024-11-13 22:06:04 -0300

    dinamica para a seleção de agendamento concluida

--- src/main/resources/static/agendamento/scriptAgendamento.js
+++ src/main/resources/static/agendamento/scriptAgendamento.js
@@ -1,12 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('#tabela1, #tabela2, #tabela3').forEach(function(tabela) {
+        tabela.style.display = 'none';
+    });
+});
+
 document.getElementById('selectMenu').addEventListener('change', function() {
-    
-    document.getElementById('tabela1').style.display = 'none';
-    document.getElementById('tabela2').style.display = 'none';
-    document.getElementById('tabela3').style.display = 'none';
+    document.querySelectorAll('#tabela1, #tabela2, #tabela3').forEach(function(tabela) {
+        tabela.style.display = 'none';
+    });
 
     var selectedValue = this.value;
     
     if (selectedValue != '0') {
-      document.getElementById('tabela' + selectedValue).style.display = 'flex';
+        document.getElementById('tabela' + selectedValue).style.display = 'table';
     }
-  });
+});
